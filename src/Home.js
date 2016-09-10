cb9b0d96e8d0680172a144a528264e7f5af7aa7e
Linsy Joyner <Linsy.Joyner@gmail.com>
2016-09-10 14:32:46 -0400

    setState error

--- src/Home.js
+++ src/Home.js
@@ -7,32 +7,48 @@ import App from './App'
 
 class Home extends Component {
 
-  terms () {
+constructor () {
+  super ();
+  this.state = {
+    listings: []
+  }
+  this.returnListings = this.returnListings.bind(this)
+}
+
+  returnListings () {
     let terms = this.refs.search.value
     axios.get('https://openapi.etsy.com/v2/listings/active.js?keywords=' +
               terms + '&limit=12&includes=Images:1&api_key=' + api_key)
-    .then(function (response) {
-      console.log(response);
+    .then((response) => {
+      console.log("response is", response);
     })
     console.log("button is working")
     console.log(this.refs.search.value)
 
     jsonp('https://openapi.etsy.com/v2/listings/active.js?keywords=' +
               terms + '&limit=12&includes=Images:1&api_key=' + api_key, null, function (err, data) {
-    if (err) {7
+    if (err) {
       console.error(err.message);
     } else {
-      console.log(data);
+      console.log("data is", data);
+      this.setState ({
+        listings: data.results
+      })
+      console.log("state is", this.state)
     }
+      // return this.state.listings.map((product) => {
+      //   return (<Listing title={product.title} key={product.id}/>)
+// }
   });
-  
-  }
+}
 
   render() {
+    // const listings = this.returnListings();
+
     return (
       <div className="Home">
       <input placeholder="search here" ref="search"/>
-      <button onClick={this.terms.bind(this)}>Search it!</button>
+      <button onClick={this.returnListings.bind(this)}>Search it!</button>
       </div>
     );
   }
